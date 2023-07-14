2c7215501f6f04f4bdc4c5725e1b577507dc1ede
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-07-14 13:48:28 +0300

    Create Category and Bind Property

--- AbbyWeb/Pages/Categories/Create.cshtml.cs
+++ AbbyWeb/Pages/Categories/Create.cshtml.cs
@@ -1,15 +1,29 @@
+using AbbyWeb.Data;
 using AbbyWeb.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace AbbyWeb.Pages.Categories
-{
+namespace AbbyWeb.Pages.Categories;
+[BindProperties]
     public class CreateModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            this._db = db ?? throw new ArgumentNullException(nameof(db));
+        }
         public Category Category { get; set; }
         public void OnGet()
         {
 
         }
+
+        public async Task<IActionResult> OnPost()
+        {
+            await _db.Category.AddAsync(Category);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
+        }
     }
-}
+
